95d1cd5b23bd82e58e1fe8eda4283a445d5a3bae
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-02-28 21:41:23 +0530

    update chat section

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Chat.css';
+import { Avatar } from '@material-ui/core';
 
 function Chat() {
+  const [seed, setSeed] = useState('');
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000))
+  }, []);
+
   return (
-    <div className='chat'></div>
+    <div className='chat'>
+      <div className='chat_header'>
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="chat_headerInfo">
+          <h3>Room name</h3>
+          <p>Last Seen at ...</p>
+        </div>
+      </div>
+      <div className='chat_body'></div>
+      <div className='chat_footer'></div>
+    </div>
   );
 }
 
