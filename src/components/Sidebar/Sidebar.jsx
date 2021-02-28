915c49d053930e62756678a4c4219c411ea5f93b
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-02-28 19:54:51 +0530

    add top buttons & minimal styling

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,24 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import { Chat, DonutLarge, MoreVert } from '@material-ui/icons';
 import React from 'react';
+
 import './Sidebar.css';
 
 function Sidebar() {
   return (
-    <div className="sidebar">
-    <h1>Sidebar</h1>
+    <div className='sidebar'>
+    <div className='sidebar_header'>
+      <Avatar />
+      <div className='sidebar_headerRight'>
+        <IconButton>
+          <DonutLarge />
+        </IconButton>
+        <Chat />
+        <MoreVert />        
+      </div>
+    </div>
+    <div className='sidebar_search'></div>
+    <div className='sidebar_chats'></div>
     </div>
   )
 };
