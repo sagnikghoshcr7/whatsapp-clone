c9ee4e2f13b23a67cbb835eff55433764d3beb31
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-02-28 21:22:25 +0530

    add chat component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 
 import Sidebar from './components/Sidebar/Sidebar';
+import Chat from './components/Chat/Chat';
 
 function App() {
   return (
     <div className="app">
       <div className="app_body">
       <Sidebar />
+      <Chat />
       </div>
     </div>
   );
---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Chat.css';
+
+function Chat() {
+  return (
+    <div className='chat'></div>
+  );
+}
+
+export default Chat;
