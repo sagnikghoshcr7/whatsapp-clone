b1ee0723a79cc6169843a9fd54492425c6ad5c2a
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-02-28 18:59:02 +0530

    add firebase initially

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCcAC8wn-YdIyVWP_v7FYLLLQ6pcVI9zMs",
+  authDomain: "whatsapp-clone-react-30c61.firebaseapp.com",
+  projectId: "whatsapp-clone-react-30c61",
+  storageBucket: "whatsapp-clone-react-30c61.appspot.com",
+  messagingSenderId: "949306881497",
+  appId: "1:949306881497:web:a09d6fa90326499e2f55c8",
+  measurementId: "G-QZWWMFNDY7"
+};
