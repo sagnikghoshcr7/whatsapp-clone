6cd7c11d931c689123659e69c6f7a861f03b1744
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-03-02 09:23:52 +0530

    create room function sync to firebase

--- src/components/SidebarChat/SidebarChat.jsx
+++ src/components/SidebarChat/SidebarChat.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Avatar } from '@material-ui/core';
 import './SidebarChat.css';
+import db from '../../firebase';
 
-function SidebarChat({ addNewChat }) {
+function SidebarChat({ id, name, addNewChat }) {
   const [seed, setSeed] = useState('');
 
   useEffect(() => {
@@ -10,10 +11,12 @@ function SidebarChat({ addNewChat }) {
   }, []);
 
   const createChat = () => {
-    const roomName = prompt("Please enter name for chat");
+    const roomName = prompt("Please enter name for chat room");
 
     if (roomName) {
-      // database stuff
+      db.collection('rooms').add({
+        name: roomName,
+      });
     }
   };
 
@@ -21,7 +24,7 @@ function SidebarChat({ addNewChat }) {
     <div className='sidebarChat'>
       <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
       <div className='sidebarChat_info'>
-        <h2>Room Name</h2>
+        <h2>{name}</h2>
         <p>Last message...</p>
       </div>
     </div>
