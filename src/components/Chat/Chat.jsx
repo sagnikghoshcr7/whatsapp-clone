42a42f2e2f3c8c5f0cd568e3ac26699a75d21135
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-03-02 14:57:33 +0530

    add chat feature

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -6,75 +6,91 @@ import { useParams } from 'react-router-dom';
 import { Avatar, IconButton } from '@material-ui/core';
 import { AttachFile, InsertEmoticon, Mic, MoreVert, SearchOutlined } from '@material-ui/icons';
 import db from '../../firebase';
+import firebase from '../../firebase';
+import { useStateValue } from '../../StateProvider';
 
 function Chat() {
-  const [input, setInput] = useState('');
-  const [seed, setSeed] = useState('');
-  const { roomId } = useParams();
-  const [roomName, setRoomName] = useState("");
+    const [input, setInput] = useState("");
+    const [seed, setSeed] = useState("");
+    const {roomId} = useParams();
+    const [roomName, setRoomName] = useState("");
+    const [messages, setMessages] = useState([]);
+    const [{user}, dispatch] = useStateValue();
 
-  useEffect(() => {
-    if (roomId) {
-      db.collection('rooms').doc(roomId).onSnapshot(snapshot => {
-        setRoomName(snapshot.data().name)
-      })
+    useEffect(()=>{
+        if(roomId){
+            db.collection('rooms').doc(roomId).onSnapshot(snapshot => {
+                setRoomName(snapshot.data().name);
+            });
+
+            db.collection('rooms').doc(roomId).collection("messages").orderBy("timestamp","asc").onSnapshot(snapshot => {
+                setMessages(snapshot.docs.map(doc => doc.data()))
+            });
+
+        }
+    },[roomId])
+
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 5000));        
+    }, [roomId]);
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+        db.collection('rooms').doc(roomId).collection('messages').add({
+            message: input,
+            name: user.displayName,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        })
+
+        setInput("");
     }
-  }, [roomId]);
 
-  useEffect(() => {
-    setSeed(Math.floor(Math.random() * 5000))
-  }, [roomId]);
-
-  const sendMessage = (e) => {
-    e.preventDefault();
-    console.log("You typed >>> ", input);
-    setInput("");
-  }
-
-  return (
-    <div className='chat'>
-      <div className='chat_header'>
-        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
-        <div className="chat_headerInfo">
-          <h3>{roomName}</h3>
-          <p>Last Seen at ...</p>
+    return (
+        <div className='chat'>
+            <div className='chat_header'>
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+                <div className='chat_headerInfo'>
+                    <h3 className='chat-room-name'>{roomName}</h3>
+                    <p className='chat-room-last-seen'>
+                        Last seen {" "}
+                        {new Date(
+                            messages[messages.length - 1]?.timestamp?.toDate()
+                        ).toUTCString()}
+                    </p>
+                </div>
+                <div className="chat_headerRight">
+                    <IconButton>
+                        <SearchOutlined/>
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert/>
+                    </IconButton>
+                    
+                </div>
+            </div>
+            <div className='chat_body'>
+                {messages.map(message => (
+                    <p className={`chat_message ${ message.name === user.displayName && 'chat_receiver'}`}>
+                        <span className="chat_name">{message.name}</span>
+                        {message.message}
+                        <span className="chat_timestemp">{new Date(message.timestamp?.toDate()).toUTCString()}</span>
+                    </p>
+                ))}
+            </div>
+            <div className='chat_footer'>
+                <InsertEmoticon />
+                <form>
+                    <input value={input} onChange={(e) => setInput(e.target.value)} type="text" placeholder="Type a message"/>
+                    <button type="submit" onClick={sendMessage}> Send a Message</button>
+                </form>
+                <Mic />
+            </div>
+            
         </div>
-        <div className="chat_headerRight">
-          <IconButton>
-            <SearchOutlined />
-          </IconButton>
-          <IconButton>
-            <AttachFile />
-          </IconButton>
-          <IconButton>
-            <MoreVert />
-          </IconButton>
-        </div>
-      </div>
-
-      <div className='chat_body'>
-        <p className={`chat_message ${true && 'chat_receiver'}`}>
-          <span className='chat_name'>Sagnik Ghosh</span>
-          Hey Guys
-          <span className='chat_timestemp'>1:30am</span>
-        </p>
-      </div>
-
-      <div className='chat_footer'>
-        <InsertEmoticon />
-        <form>
-          <input
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            placeholder="Type a message"
-            type='text'
-          />
-          <button onClick={sendMessage} type='submit'>Send a message</button>
-        </form>
-        <Mic />
-      </div>
-    </div>
-  );
+    )
 }
 
 export default Chat;
